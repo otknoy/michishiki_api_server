d32c5339291e08654c554aeda6db3160bc6a29b1
otknoy <otknoy@gmail.com>
2014-07-29 16:21:39 +0900

    Minor change

--- cgi_test.py
+++ cgi_test.py
@@ -1,24 +1,10 @@
 #!/usr/bin/env python
-import sqlite3
-
-def cgi_header():
-    print 'Content-Type: text/html'
-    print
-
-def init_db(path):
-    con = sqlite3.connect('db/michishiki.sqlite3')
-    sql = u"""
-"""
-    
 
 if __name__ == '__main__':
-    import cgitb
-    cgitb.enable()
-
-    cgi_header()
+    import utils
+    utils.cgi_header()
     
     data = {'a': 1, 'b': 2, 'c': 3}
-
     import json
     print json.dumps(data)
 
