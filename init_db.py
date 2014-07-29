217e0c26d97920d904ea2da9eb7259c6b936eeab
otknoy <otknoy@gmail.com>
2014-07-29 16:19:48 +0900

    Read sql statement from file

--- init_db.py
+++ init_db.py
@@ -7,15 +7,6 @@ def create_db(sql, filename):
     con.close()
 
 if __name__ == '__main__':
-    sql = u'''create table posts (
-  id integer primary key autoincrement,
-  latitude real,
-  longitude real,
-  title text,
-  comment text,
-  posted_by text,
-  created_at integer
-);
-'''
+    sql = open('data/import.sql').read()
     import config    
     create_db(sql, config.db_path)
