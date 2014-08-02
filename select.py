bb79f93892c15a08277ff5e77c316bc94238129e
otknoy <otknoy@gmail.com>
2014-08-02 21:04:14 +0900

    Minor change

--- select.py
+++ select.py
@@ -5,8 +5,7 @@ import sqlite3
 
 import config
 
-def select():
-    sql = u'select * from posts'
+def select(sql):
     conn = sqlite3.connect(config.db_path, isolation_level=None)
     conn.row_factory = sqlite3.Row
     cur = conn.cursor()
@@ -15,13 +14,18 @@ def select():
     conn.close()
     return results
 
+def select_all():
+    sql = u'select * from posts'
+    return select(sql)
+
+
 def rows2dict(rows):
     '''list of sqlite3.Row to dict'''
     return [dict(zip(r.keys(), r)) for r in rows]
 
 
 if __name__ == '__main__':
-    result = select()
+    result = select_all()
 
     import utils
     utils.cgi_header()
